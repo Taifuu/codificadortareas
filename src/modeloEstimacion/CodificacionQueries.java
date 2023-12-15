d506396424a968c99c599465d15fa4c88d323c2c
Fabian Roldan <froldan@cognisys.com.ar>
2023-12-15 18:06:05 -0300

    Se mejora el recupero de parametros out y se cambia el formato

--- src/modeloEstimacion/CodificacionQueries.java
+++ src/modeloEstimacion/CodificacionQueries.java
@@ -1,8 +1,5 @@
 package modeloEstimacion;
 
-import java.io.PipedInputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,7 +11,10 @@ public class CodificacionQueries {
 	public static final String PIPE_PUNTO = ".";
 	public static final String PIPE_COMA = ",";
 	public static final String PIPE_DOBLE_PUNTO = ":";
-	public static final String PIPE_PARENTESIS_DERECHO = ")";
+	public static final char PIPE_PARENTESIS_DERECHO = ')';
+	public static final char PIPE_PARENTESIS_IZQUIERDO = '(';
+	public static final String PIPE_PARENTESIS_DERECHO_STRING = ")";
+	public static final String PIPE_PARENTESIS_IZQUIERDO_STRING = ")";
 	public static final String PIPE_PUNTO_COMA = ";";
 	public static final String PATRON_PARAMETRO_IN = "(\\S+)";
 	public static final String PIPE_BARRA_N = "\n";
@@ -28,11 +28,13 @@ public class CodificacionQueries {
 		}
 		int posFrom = query.indexOf(CLAVE_FROM);
 		String parametrosSalidaString = query.substring(posSelect, posFrom);
+		parametrosSalidaString = recortarPalabrasEntreParentesis(parametrosSalidaString);
 		String[] parametrosSpliteados = parametrosSalidaString.split(PIPE_COMA);
 		int limite = parametrosSpliteados.length;
 		for (int i = 0; i < limite; i++) {
 			String unParametro = parametrosSpliteados[i].trim();
 			String temp = unParametro;
+			
 			if(temp.contains(PIPE_PUNTO)) {
 				temp = temp.substring(temp.indexOf(PIPE_PUNTO)+1);
 			}
@@ -40,8 +42,7 @@ public class CodificacionQueries {
 				String[] parSplit = temp.split(PIPE_ESPACIO);
 				temp = parSplit[parSplit.length-1];
 			}
-			temp = QueryTexto.TEMPLATE_PARAMETRO_DE_SALIDA.replace(QueryTexto.NOMBRE_PARAMETRO_SALIDA, temp);
-			temp = temp.replace(QueryTexto.POSICION_PARAMETRO_SALIDA, ((i+1)+""));
+			temp = QueryTexto.TEMPLATE_PARAMETRO_DE_SALIDA.replace(QueryTexto.NOMBRE_PARAMETRO_SALIDA, temp.toUpperCase());
 			if(i+1 < limite) {
 				template.append(temp+"\n");
 			} else {
@@ -49,7 +50,7 @@ public class CodificacionQueries {
 			}
 		}
 		
-		return QueryTexto.TEMPLATE_INTERFACE_OUT_PARAMS.replace(QueryTexto.PARAMETRO_DE_SALIDA, template.toString());
+		return template.toString();
 	}
 	
 	public static String getParametrosEntrada(String query) {
@@ -63,14 +64,14 @@ public class CodificacionQueries {
 				if(palabra.contains(PIPE_COMA)) {
 					palabra = palabra.replace(PIPE_COMA, "");
 				}
-				if(palabra.contains(PIPE_PARENTESIS_DERECHO)) {
-					palabra = palabra.replace(PIPE_PARENTESIS_DERECHO, "");
+				if(palabra.contains(PIPE_PARENTESIS_DERECHO_STRING)) {
+					palabra = palabra.replace(PIPE_PARENTESIS_DERECHO_STRING, "");
 				}
 				if(palabra.contains(PIPE_PUNTO_COMA)) {
 					palabra = palabra.replace(PIPE_PUNTO_COMA, "");
 				}
 				if(template.indexOf(palabra) < 0) {
-					template.append(QueryTexto.TEMPLATE_PARAMETRO_DE_ENTRADA.replace(QueryTexto.NOMBRE_PARAMETRO_ENTRADA, palabra));
+					template.append(QueryTexto.TEMPLATE_PARAMETRO_DE_ENTRADA.replace(QueryTexto.NOMBRE_PARAMETRO_ENTRADA, palabra.toUpperCase()));
 				} else {
 					continue;
 				}
@@ -79,7 +80,7 @@ public class CodificacionQueries {
 				template.append("\n");
 			}
 		}
-		return QueryTexto.TEMPLATE_INTERFACE_IN_PARAMS.replace(QueryTexto.PARAMETRO_DE_ENTRADA, template.toString());
+		return template.toString();
 	}
 	
 	public static String decodeQuery(String query) {
@@ -112,6 +113,94 @@ public class CodificacionQueries {
 		output = output.replace(QueryTexto.QUERY_COMPLETA, queryCompleta.toString());
 		
 		return output;
+	}
+	
+	private static String recortarPalabrasEntreParentesis(String parametrosSalidaString) {
+		StringBuilder output = new StringBuilder(parametrosSalidaString);
+		boolean parentesis = parametrosSalidaString.contains(PIPE_PARENTESIS_IZQUIERDO_STRING);
+		do {
+			int contParIzq = 0;
+			int posPrimerParIzq = 0;
+			int contParDer = 0;
+			//Si tengo parentesis izquierdo
+			if(parentesis) {
+				for (int i = 0; i < output.length(); i++) {
+					char c = output.charAt(i);
+					//Reviso cuantos tengo y los cuento
+					if(PIPE_PARENTESIS_IZQUIERDO == c) {
+						if(contParIzq == 0) {
+							posPrimerParIzq = i;
+						}
+						contParIzq += 1;
+					}
+					//Cuando encuentro el primer parentesis derecho
+					if(PIPE_PARENTESIS_DERECHO == c) {
+						for (int j = 0; j < output.length(); j++) {
+							char z = output.charAt(j);
+							//Reviso cuantos hay y los cuento
+							if(PIPE_PARENTESIS_DERECHO == z) {
+								contParDer += 1;
+							}
+							//Cuando la cantidad sea la misma
+							if(contParIzq <= contParDer) {
+								//Recorto el string para eliminar toda esa parte que no sirve
+								output.delete(posPrimerParIzq, j+2);
+								break;
+							}
+						}
+					}
+				}
+				//Reviso si hay más partes innecesarias para cortar
+				parentesis = output.toString().contains(PIPE_PARENTESIS_IZQUIERDO_STRING);
+			}
+			/*if(parentesis) {
+				for (int i = 0; i < parametrosSalidaString.length(); i++) {
+					char c = parametrosSalidaString.charAt(i);
+					//Reviso cuantos tengo y los cuento
+					if(PIPE_PARENTESIS_IZQUIERDO == c) {
+						contParIzq += 1;
+					}
+					//Cuando encuentro el primer parentesis derecho
+					if(PIPE_PARENTESIS_DERECHO == c) {
+						for (int j = 0; j < parametrosSalidaString.length(); j++) {
+							char z = parametrosSalidaString.charAt(j);
+							//Reviso cuantos hay y los cuento
+							if(PIPE_PARENTESIS_DERECHO == z) {
+								contParDer += 1;
+							}
+							//Cuando la cantidad sea la misma
+							if(contParIzq <= contParDer) {
+								//Recorto el string para eliminar toda esa parte que no sirve
+								parametrosSalidaString = parametrosSalidaString.substring(j+2);
+								//Y reviso si hay más parametros de salida
+								if(parametrosSalidaString.contains(PIPE_COMA)) {
+									//Si los hay, los guardo en una variable temporal
+									String par = parametrosSalidaString.substring(0, parametrosSalidaString.indexOf(PIPE_COMA)+1);
+									if(par.contains(PIPE_PARENTESIS_IZQUIERDO_STRING)) {
+										par = recortarPalabrasEntreParentesis(par);
+									}
+									output.append(par);
+									parametrosSalidaString = parametrosSalidaString.substring(parametrosSalidaString.indexOf(PIPE_COMA)+1);
+									break;
+								} else {
+									//Si es el último o no hay más, también los guardo en la variable
+									output.append(parametrosSalidaString);
+									break;
+								}
+							}
+						}
+					}
+				}
+				//Reviso si hay más partes innecesarias para cortar
+				parentesis = parametrosSalidaString.contains(PIPE_PARENTESIS_IZQUIERDO_STRING);
+			}*/
+		} while (parentesis);
+		//Si en principio no había parentesis entonces devuelvo lo que entro, sino devuelvo lo que tengo en output
+		if(output.length() > 0) {
+			return output.toString();
+		} else {
+			return parametrosSalidaString;
+		}
 	}
 	
 	public static void main(String[] args) {
--- src/modeloEstimacion/QueryTexto.java
+++ src/modeloEstimacion/QueryTexto.java
@@ -3,11 +3,8 @@ package modeloEstimacion;
 public abstract class QueryTexto {
 	public static final String IN_PARAMS = "[IN_PARAMS]";
 	public static final String OUT_PARAMS = "[OUT_PARAMS]";
-	public static final String PARAMETRO_DE_SALIDA = "[PARAMETRO_DE_SALIDA]";
 	public static final String NOMBRE_PARAMETRO_SALIDA = "[NOMBRE_PARAMETRO_SALIDA]";
-	public static final String POSICION_PARAMETRO_SALIDA = "[POSICION_PARAMETRO_SALIDA]";
 	public static final String NOMBRE_PARAMETRO_ENTRADA = "[NOMBRE_PARAMETRO]";
-	public static final String PARAMETRO_DE_ENTRADA = "[PARAMETRO_DE_ENTRADA]";
 	public static final String QUERY_COMPLETA = "[QUERY_COMPLETA]";
 	public static final String NOMBRE_INTERFACE = "[NOMBRE_INTERFACE]";
 
@@ -20,19 +17,11 @@ public abstract class QueryTexto {
 	public static final String TEMPLATE_INTERFACE = "interface "+ NOMBRE_QUERY +" {\n" + 
 			"\tString QUERY = "+ QUERY_COMPLETA +";\n" + 
 			"\n" + 
-			"\t"+ IN_PARAMS +"\n" + 
-			"\t"+ OUT_PARAMS +"\n" + 
+			IN_PARAMS +"\n" + 
+			OUT_PARAMS +"\n" + 
 			"}";
 	
-	public static final String TEMPLATE_INTERFACE_IN_PARAMS = "interface IN_PARAMS {\n" + 
-			""+ PARAMETRO_DE_ENTRADA +"\n" + 
-			"\t}";
+	public static final String TEMPLATE_PARAMETRO_DE_ENTRADA = "\tString IN_"+ NOMBRE_PARAMETRO_ENTRADA +" = \""+ NOMBRE_PARAMETRO_ENTRADA +"\";";
 	
-	public static final String TEMPLATE_PARAMETRO_DE_ENTRADA = "\t\tString "+ NOMBRE_PARAMETRO_ENTRADA +" = \""+ NOMBRE_PARAMETRO_ENTRADA +"\";";
-	
-	public static final String TEMPLATE_INTERFACE_OUT_PARAMS = "interface OUT_PARAMS {\n" + 
-			""+ PARAMETRO_DE_SALIDA +"\n" + 
-			"\t}";
-	
-	public static final String TEMPLATE_PARAMETRO_DE_SALIDA = "\t\tint "+ NOMBRE_PARAMETRO_SALIDA +" = "+ POSICION_PARAMETRO_SALIDA +";";
+	public static final String TEMPLATE_PARAMETRO_DE_SALIDA = "\tString OUT_"+ NOMBRE_PARAMETRO_SALIDA +" = \""+ NOMBRE_PARAMETRO_SALIDA +"\";";
 }
